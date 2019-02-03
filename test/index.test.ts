a9ff056c3e6785d1d8a43846132cf02de2915d9a
Tim van Scherpenzeel <timvanscherpenzeel@gmail.com>
2019-02-03 11:49:31 +0100

    added IE / Edge checks

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,21 +1,52 @@
 // Vendor
 import { DetectUA } from '../src';
 
+// Data
+import { USER_AGENT_IE10, USER_AGENT_IE11, USER_AGENT_EDGE12, USER_AGENT_EDGE13 } from './data';
+
 // Suite
 describe('DetectUA', () => {
   it('should instantiate a new instance of DetectUA', () => {
     expect.assertions(1);
 
-    const ua = new DetectUA();
+    const UA = new DetectUA();
 
-    expect(ua).toBeInstanceOf(DetectUA);
+    expect(UA).toBeInstanceOf(DetectUA);
   });
 
   it('should accept a forced user agent string', () => {
     expect.assertions(1);
 
-    const ua = new DetectUA('Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko');
+    const UA = new DetectUA('Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko');
 
-    expect(ua.userAgent).toEqual('mozilla/5.0 (windows nt 6.3; trident/7.0; rv:11.0) like gecko');
+    expect(UA.userAgent).toEqual('mozilla/5.0 (windows nt 6.3; trident/7.0; rv:11.0) like gecko');
+  });
+
+  it('should match IE10 / IE11 user agent with Internet Explorer', () => {
+    expect.assertions(4);
+
+    const UA_IE10 = new DetectUA(USER_AGENT_IE10);
+
+    expect(UA_IE10.isInternetExplorer).toEqual(true);
+    expect(UA_IE10.isEdge).toBeFalsy();
+
+    const UA_IE11 = new DetectUA(USER_AGENT_IE11);
+
+    expect(UA_IE11.isInternetExplorer).toEqual(true);
+    expect(UA_IE11.isEdge).toBeFalsy();
+  });
+
+  it('should match EGDE12 / EDGE 13 user agent with Edge', () => {
+    expect.assertions(4);
+
+    const UA_EDGE12 = new DetectUA(USER_AGENT_EDGE12);
+
+    expect(UA_EDGE12.isEdge).toEqual(true);
+    expect(UA_EDGE12.isInternetExplorer).toBeFalsy();
+
+    const UA_EDGE13 = new DetectUA(USER_AGENT_EDGE13);
+
+    expect(UA_EDGE13.isEdge).toEqual(true);
+    expect(UA_EDGE13.isInternetExplorer).toBeFalsy();
   });
 });
