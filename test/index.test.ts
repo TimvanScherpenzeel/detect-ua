1d3a49600153558ae725ab324e6142a96e31bc2f
Tim van Scherpenzeel <timvanscherpenzeel@gmail.com>
2020-02-12 15:11:27 +0100

    clean up test

--- test/index.test.ts
+++ test/index.test.ts
@@ -5,8 +5,8 @@ import { DetectUA } from '../src';
 import { USER_AGENT_STRINGS } from './data';
 
 // Suite
-describe('DetectUA', () => {
-  it('should instantiate a new instance of DetectUA', () => {
+describe('DetectUA', (): void => {
+  it('should instantiate a new instance of DetectUA', (): void => {
     expect.assertions(1);
 
     const UA = new DetectUA();
@@ -14,7 +14,7 @@ describe('DetectUA', () => {
     expect(UA).toBeInstanceOf(DetectUA);
   });
 
-  it('should accept a forced user agent string', () => {
+  it('should accept a forced user agent string', (): void => {
     expect.assertions(1);
 
     const UA = new DetectUA('Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko');
@@ -22,8 +22,8 @@ describe('DetectUA', () => {
     expect(UA.userAgent).toEqual('Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko');
   });
 
-  Object.keys(USER_AGENT_STRINGS).forEach((browser: string) => {
-    USER_AGENT_STRINGS[browser].forEach((entry: any) => {
+  Object.keys(USER_AGENT_STRINGS).forEach((browser: string): void => {
+    USER_AGENT_STRINGS[browser].forEach((entry: any): void => {
       it('should correctly detect the browser and browser version based on the detected user agent', () => {
         expect.assertions(1);
 
